ffce027f7be90c4e8573227573680f2d347ed348
Tianj0o <2576099053@qq.com>
2021-04-05 14:36:07 +0800

    change

--- src/components/user-con.vue
+++ src/components/user-con.vue
@@ -25,16 +25,21 @@ export default {
     }
 }
 </script>
-<style>
+<style lang='scss' scoped>
 .user-con {
     position: relative;
     margin-top: 20px;
     border: whitesmoke solid 2px;
-}
-.user-contents {
+    transition: all 0.25s ease;
+    &:hover {
+    transform: scale(1.1, 1.1);
+  }
+    .user-contents {
    margin: 10px;
    color: rgb(143, 51, 155);
    font-size: large;
 }
+}
+
 
 </style>
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+moudule.exports = {
+    css:{
+        loaderOptions:{
+            sass:{
+                additionalData:'@import "@/style/base.scss";'
+            }
+        }
+    }
+}
